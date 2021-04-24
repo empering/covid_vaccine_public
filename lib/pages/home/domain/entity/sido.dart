6efa692bd3c92451fa4e965c6782cdf6ba06d579
empering <empering@naver.com>
2021-04-24 22:08:15 +0900

    시도 enum 생성

---
+++ lib/pages/home/domain/entity/sido.dart
@@ -0,0 +1,98 @@
+enum SidoEnum {
+  ALL,
+  SEOUL,
+  BUSAN,
+  DAEGU,
+  INCHEON,
+  GWANJU,
+  DAEJEON,
+  ULSAN,
+  SEJONG,
+  GYEONGGI,
+  GANGWON,
+  CHUNGBUK,
+  CHUNGNAM,
+  JEONBUK,
+  JEONNAM,
+  GYEONGBUK,
+  GYEONGNAM,
+  JEJU,
+  ETC,
+}
+
+class Sido {
+  static String getSidoName(SidoEnum sidoEnum, {bool isShort = false}) {
+    String sidoName = '';
+    switch (sidoEnum) {
+      case SidoEnum.ALL:
+        sidoName = '전국';
+        break;
+      case SidoEnum.SEOUL:
+        sidoName = '서울특별시';
+        break;
+      case SidoEnum.BUSAN:
+        sidoName = '부산광역시';
+        break;
+      case SidoEnum.DAEGU:
+        sidoName = '대구광역시';
+        break;
+      case SidoEnum.INCHEON:
+        sidoName = '인천광역시';
+        break;
+      case SidoEnum.GWANJU:
+        sidoName = '광주광역시';
+        break;
+      case SidoEnum.DAEJEON:
+        sidoName = '대전광역시';
+        break;
+      case SidoEnum.ULSAN:
+        sidoName = '울산광역시';
+        break;
+      case SidoEnum.SEJONG:
+        sidoName = '세종특별자치시';
+        break;
+      case SidoEnum.GYEONGGI:
+        sidoName = '경기도';
+        break;
+      case SidoEnum.GANGWON:
+        sidoName = '강원도';
+        break;
+      case SidoEnum.CHUNGBUK:
+        sidoName = '충청북도';
+        break;
+      case SidoEnum.CHUNGNAM:
+        sidoName = '충청남도';
+        break;
+      case SidoEnum.JEONBUK:
+        sidoName = '전라북도';
+        break;
+      case SidoEnum.JEONNAM:
+        sidoName = '전라남도';
+        break;
+      case SidoEnum.GYEONGBUK:
+        sidoName = '경상북도';
+        break;
+      case SidoEnum.GYEONGNAM:
+        sidoName = '경상남도';
+        break;
+      case SidoEnum.JEJU:
+        sidoName = '제주특별자치도';
+        break;
+      case SidoEnum.ETC:
+        sidoName = '기타';
+    }
+
+    return isShort ? sidoName.substring(0, 2) : sidoName;
+  }
+
+  static SidoEnum getSidoEnum(String sidoName) {
+    var sidoEnum = SidoEnum.ETC;
+    SidoEnum.values.forEach((s) {
+      if (getSidoName(s) == sidoName) {
+        sidoEnum = s;
+      }
+    });
+
+    return sidoEnum;
+  }
+}
